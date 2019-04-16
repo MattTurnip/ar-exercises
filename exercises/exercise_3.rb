01894852ca1117a96147c2be8984b65e33c2b6fe
MattTurnip <mattadavey@gmail.com>
2019-04-16 16:07:24 +0000

    Add 3 more stores, Add var for men's store, Add var for broke stores with women's apparel

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -8,5 +8,3 @@ puts "----------"
 # Your code goes here ...
 @store3 = Store.find(3)
 @store3.destroy
-
-binding.pry
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,15 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create!(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create!(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create!(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: false)
+
+@mens_stores = Store.where({mens_apparel: true, womens_apparel: false})
+
+@mens_stores.each do |store|
+  puts "name of men's store: #{store.name}"
+  puts "annual revenue that store: #{store.annual_revenue}"
+end
+
+@broke_unisex_stores = Store.where({womens_apparel: true}).having('annual_revenue < 1000000').group('id')
