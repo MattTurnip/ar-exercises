5786a858f5af307901009583fc30d7ec28d5ef36
MattTurnip <mattadavey@gmail.com>
2019-04-16 18:22:49 +0000

    Remove Comment

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,14 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
-# Store.create!(name: "Oshawa", annual_revenue: 224000, mens_apparel: false, womens_apparel: false) 
 puts "==============Please enter a store name==============f"
 
 user_store = $stdin.gets.chomp
 
 store = Store.create(name: user_store)
 
-binding.pry
 store.errors.full_messages.each do |e|
     puts e
 end
